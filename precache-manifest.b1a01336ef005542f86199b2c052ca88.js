8fadd4b2de670b7593a4607daec75dc316bed50b
RoiexLee <luoyixuan1029@gmail.com>
2025-01-17 05:07:39 +0000

    Deploying to gh-pages from @ RoiexLee/mdas@cc6b97b6c9c3e7144d6ecbc4cfe9f7c884a9da9d 🚀

---
+++ precache-manifest.b1a01336ef005542f86199b2c052ca88.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "73b66873874a5642a9b1",
+    "url": "css/app.2741e926.css"
+  },
+  {
+    "revision": "73b66873874a5642a9b1",
+    "url": "js/app.96cc58f7.js"
+  },
+  {
+    "revision": "a79b2e3318ac5a4c7d87",
+    "url": "js/chunk-2d216257.58872a85.js"
+  },
+  {
+    "revision": "71e255e4308a13cc5611",
+    "url": "css/chunk-vendors.bc91fb24.css"
+  },
+  {
+    "revision": "71e255e4308a13cc5611",
+    "url": "js/chunk-vendors.04141396.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "e002ea3c0394752d230ae9d5c4722955",
+    "url": "img/logo.e002ea3c.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "db1f7f8e5b9e8038816525a385d4046a",
+    "url": "index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "185288d13ed8e9d745bd279ea34667bf",
+    "url": "img/brand/blue.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "img/brand/favicon.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "6fafe4baca9d50d61a898c84ade7afa3",
+    "url": "img/brand/white.png"
+  },
+  {
+    "revision": "8fa3077dab250632f2bc169026ce974d",
+    "url": "img/exm/rorschach0.png"
+  },
+  {
+    "revision": "f1894ac96a996e483a841a521bd7f017",
+    "url": "img/exm/rorschach1.png"
+  },
+  {
+    "revision": "016e695d54a58ca91a60d151985b7b2d",
+    "url": "img/exm/rorschach2.png"
+  },
+  {
+    "revision": "3c96a13a127c932eaaa6f485b6f7029a",
+    "url": "img/exm/rorschach3.png"
+  },
+  {
+    "revision": "7fb2687513b67fa758a56f2dec66173f",
+    "url": "img/exm/rorschach4.png"
+  },
+  {
+    "revision": "674c8547fa9a14b73c9cb3014ef40140",
+    "url": "img/exm/rorschach5.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "img/theme/landing.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "img/theme/profile.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "img/theme/promo-1.png"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "66618a418175ddf2ac8c47a241d327a8",
+    "url": "img/theme/team-4-800x800.jpg"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "img/theme/team-2-800x800.jpg"
+  }
+];
