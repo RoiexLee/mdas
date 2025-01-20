0f3441d33dae39000d72dfded0138d1130dbaf1f
RoiexLee <2648281162@xiaomi.com>
2025-01-20 22:55:19 +0800

    add userInfo, change CLEAR_TOKEN to CLEAR

--- src/store/index.js
+++ src/store/index.js
@@ -7,21 +7,29 @@ Vue.use(Vuex);
 export default new Vuex.Store({
   state: {
     token: sessionStorage.getItem("token") || null,
+    userInfo: JSON.parse(sessionStorage.getItem("userInfo")) || null,
   },
   getters: {
     isAuthenticated: (state) => state.token !== null,
+    userInfo: (state) => state.userInfo,
   },
   mutations: {
     SET_TOKEN(state, token) {
       state.token = token;
       sessionStorage.setItem("token", token);
     },
-    CLEAR_TOKEN(state) {
+    SET_USER_INFO(state, userInfo) {
+      state.userInfo = userInfo;
+      sessionStorage.setItem("userInfo", JSON.stringify(userInfo));
+    },
+    CLEAR(state) {
       state.token = null;
+      state.userInfo = null;
       sessionStorage.removeItem("token");
+      sessionStorage.removeItem("userInfo");
     },
   },
   modules: {
-    modal
-  }
+    modal,
+  },
 });
